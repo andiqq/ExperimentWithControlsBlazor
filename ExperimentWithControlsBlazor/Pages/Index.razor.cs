2421101db40c2c5ead2199b44ba3ee4616b25fac
Dr. Andreas Plagens <andreas@plagens.eu>
2021-04-24 22:01:05 +0200

    * .gitignore: final
    * ExperimentWithControlsBlazor.sln:
    * App.razor:
    * Program.cs:
    * _Imports.razor:
    * Index.razor:
    * index.html:
    * favicon.ico:
    * app.css:
    * Index.razor.cs:
    * NavMenu.razor:
    * MainLayout.razor:
    * NavMenu.razor.css:
    * MainLayout.razor.css:
    * launchSettings.json:
    * weather.json:
    * README.md:
    * ExperimentWithControlsBlazor.csproj:
    * FONT-LICENSE:
    * ICON-LICENSE:
    * bootstrap.min.css:
    * bootstrap.min.css.map:
    * open-iconic.eot:
    * open-iconic.otf:
    * open-iconic.svg:
    * open-iconic.ttf:
    * open-iconic.woff:
    * open-iconic-bootstrap.min.css:

---
+++ ExperimentWithControlsBlazor/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace ExperimentWithControlsBlazor.Pages
+{
+    public partial class Index
+    {
+        private string DisplayValue = "";
+
+        private void UpdateValue(ChangeEventArgs e)
+        {
+            DisplayValue = e.Value.ToString();
+        }
+
+        private void UpdateNumericValue(ChangeEventArgs e)
+        {
+            if (int.TryParse(e.Value.ToString(), out int result))
+            {
+                DisplayValue = e.Value.ToString();
+            }
+        }
+
+        private void ButtonClick(string displayValue)
+        {
+            DisplayValue = displayValue;
+        }
+    }
+}
